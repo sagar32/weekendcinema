5664f541aef594b567fd42b997f866a3b7c6e87d
karamala ganesh <gkaramala@dstworldwideservices.com>
2015-08-26 14:13:03 +0530

    Update to home page.

--- public/js/app.js
+++ public/js/app.js
@@ -7,12 +7,28 @@ myModule.controller("homeCtrl",function($scope){
 		currentDate : new Date(),
 		homeTitle :'Home',
 		cinemaTitle:'Cinema',
+		weekendReportTitle:'Weekend Report',
 		languageTitle :'తెలుగు',
 		color : 'color'+parseInt( new Date().getDate()%7),
 		newsTitle : 'Latest News',
 		showNewsLinks : true,
-		showLinkContent:false
+		showLinkContent:false,
+		home:true,
+		cinema:false,
+		weekendReport:false
 	};
+	
+	$scope.menuItemClick = function(selectedItem){
+		$scope.pageData.home = false;
+		$scope.pageData.cinema = false;
+		$scope.pageData.weekendReport = false;
+		switch( selectedItem ){
+		  case 'home': $scope.pageData.home = true;break;
+		  case 'cinema': $scope.pageData.cinema = true;break;
+		  case 'weekendReport': $scope.pageData.weekendReport = true;break;
+		}
+		
+	}
 	
     $scope.showOrHideNews = function(number){
 
@@ -23,23 +39,21 @@ myModule.controller("homeCtrl",function($scope){
 
 	$scope.changeLanguage = function(){
 
-		if ( $scope.pageData.languageTitle!='English' ) {
+		if ( $scope.pageData.languageTitle!='English' ){
+			
 			$scope.pageData.languageTitle ='English'; 
-			$scope.pageData.newsTitle = 'లేటెస్ట్ న్యూస్';
 			$scope.pageData.homeTitle = 'హోం';
 			$scope.pageData.cinemaTitle ='సినిమా' ;
+			$scope.pageData.weekendReportTitle = 'వీకెండ్ రిపోర్ట్'
 		}
 		else{
-			 $scope.pageData =  	{
-				currentDate : new Date(),
-				homeTitle :'Home',
-				cinemaTitle:'Cinema',
-				languageTitle :'తెలుగు',
-				color : 'color'+parseInt( new Date().getDate()%7),
-				newsTitle : 'Latest News',
-				showNewsLinks : true,
-				showLinkContent:false
-			};
+			
+			 $scope.pageData.currentDate = new Date(),
+			 $scope.pageData.homeTitle = 'Home',
+			 $scope.pageData.cinemaTitle = 'Cinema',
+			 $scope.pageData.weekendReportTitle = 'Weekend Report',
+			 $scope.pageData.languageTitle = 'తెలుగు',
+			 $scope.pageData.color = 'color'+parseInt( new Date().getDate()%7)
 		}
 		
 	}
